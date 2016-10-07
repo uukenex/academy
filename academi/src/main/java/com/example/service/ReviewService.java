1bdc24f0808533bf7fcfc103c077ddc985f2d1cc
1-718-8 <1-718-8@23-717-8-PC>
2016-10-07 13:20:41 +0900

    prac

--- academi/src/main/java/com/example/service/ReviewService.java
+++ academi/src/main/java/com/example/service/ReviewService.java
@@ -18,7 +18,7 @@ public interface ReviewService {
 	public int deleteReview(int reviewNo);
 
 	// 4x4 페이지 로딩 추천먼저 띄우고 나머지12개를띄움
-	public void reviewListByPage(int page);
+	public void reviewListByPage(int page); 
 
 	// 이름검색 추천먼저 띄우고 나머지12개를띄움
 	public void searchReviewByName(String reviewTitle, int page);
