b33f0d65c38ee7cce628e44bb18f0ea3d47449f5
1-718-8 <1-718-8@23-717-8-PC>
2016-10-11 13:24:28 +0900

    2016-10-11 13:25 사진업로드 왜안됨..

--- academi/src/main/java/com/example/controller/THController.java
+++ academi/src/main/java/com/example/controller/THController.java
@@ -7,5 +7,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class THController {
-
+	@RequestMapping(value="/photoUpload",method=RequestMethod.POST)
+	public String board(Model model){
+		return "file_uploader_html";
+	}
+	
+	@RequestMapping(value="/multiplePhotoUpload",method=RequestMethod.POST)
+	public String board2(Model model){
+		return "file_uploader_html5";
+	}
+	
 }
